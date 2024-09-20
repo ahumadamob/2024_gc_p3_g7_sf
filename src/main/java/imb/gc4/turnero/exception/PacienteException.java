39b3514b631d7cd8a2a8951a8fb50eeae559059b
julilescano <julietalescano94@gmail.com>
2024-09-20 18:39:49 -0300

    Persistencia completa paciente

---
+++ src/main/java/imb/gc4/turnero/exception/PacienteException.java
@@ -0,0 +1,8 @@
+package imb.gc4.turnero.exception;
+
+public class PacienteException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	public PacienteException(String message) {
+        super(message);
+    }
+}
