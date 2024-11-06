b5f4209758d798bc4b276f1e3f65d494ecb99fa4
julilescano <julietalescano94@gmail.com>
2024-11-06 18:25:03 -0300

    Seguridad en Spring

---
+++ src/main/java/imb/gc4/turnero/controller/AuthController.java
@@ -0,0 +1,38 @@
+package imb.gc4.turnero.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import imb.gc4.turnero.dto.JwtResponse;
+import imb.gc4.turnero.dto.LoginRequest;
+import imb.gc4.turnero.security.jwt.JwtUtils;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        return ResponseEntity.ok(new JwtResponse(jwt));
+    }
+}
